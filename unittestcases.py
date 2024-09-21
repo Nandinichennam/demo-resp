4b16dd3517a221202aa82b544759985afc7373b3
Nandinichennam <chennamnandini2003@gmail.com>
2024-09-21 10:49:06 +0530

    Create unittestcases.py

---
+++ unittestcases.py
@@ -0,0 +1,11 @@
+import unittest 
+from multiply import multiplication
+class multiplyTestcase(unittest,Testcase):
+  def test_1(self):
+    result=multiplication(3,4)
+    self.assertEqual(result,12)
+    def tetst_2(self):
+      result=multiplication(-3,4)
+      self.assertEqualresult(result,12)
+      if__name_=='__main__':
+      unittest.main()
